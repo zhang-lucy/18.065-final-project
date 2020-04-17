0c0e6a711cd6a878f1b80243fc81fdf07ca0a36b
zhang-lucy <zhangly@mit.edu>
2020-04-17 10:22:50 -0700

    function

--- preprocessing.py
+++ preprocessing.py
@@ -4,19 +4,20 @@ import os
 import networkx as nx
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('soc-redditHyperlinks-body.tsv', sep='\t')
+def preprocess():
+	df = pd.read_csv('soc-redditHyperlinks-body.tsv', sep='\t')
 
-## splitting properties ##
-properties = []
-with open('properties.txt') as fp:
-    line = fp.readline()
-    line = fp.readline()
-    while line and len(line) > 0:
-        line = fp.readline()
-        properties.append(line[3:-1])
-        
-propsDf = df["PROPERTIES"].str.split(",", expand=True) 
-for i in range(len(properties)):
-    prop = properties[i]
-    df[prop] = propsDf[i].astype("float64")
-df.drop(columns="PROPERTIES",inplace=True)
+	## splitting properties ##
+	properties = []
+	with open('properties.txt') as fp:
+	    line = fp.readline()
+	    line = fp.readline()
+	    while line and len(line) > 0:
+	        line = fp.readline()
+	        properties.append(line[3:-1])
+	        
+	propsDf = df["PROPERTIES"].str.split(",", expand=True) 
+	for i in range(len(properties)):
+	    prop = properties[i]
+	    df[prop] = propsDf[i].astype("float64")
+	df.drop(columns="PROPERTIES",inplace=True)
